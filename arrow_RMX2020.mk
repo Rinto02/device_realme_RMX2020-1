0723b9b77373d1dc6b780b76967b2fae944e9b00
凛岡村リント <138510229+Rinto02@users.noreply.github.com>
2024-10-02 06:23:29 +0600

    RMX2020: ArrowOS-Extended
    Signed-off-by: 凛岡村リント <138510229+Rinto02@users.noreply.github.com>

--- arrow_RMX2020.mk
+++ arrow_RMX2020.mk
@@ -12,12 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
-# Inherit some common AfterLife stuff.
-TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := afterlife_RMX2020
+PRODUCT_NAME := arrow_RMX2020
 PRODUCT_DEVICE := RMX2020
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2020
@@ -27,12 +26,9 @@ PRODUCT_SYSTEM_MODEL := RMX2020
 PRODUCT_SYSTEM_NAME := RMX2020
 PRODUCT_SYSTEM_DEVICE := RMX2020
 
-# AfterLife additions
-BUILD_AOSP_CAMERA := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_BLUR := true
-AFTERLIFE_CORE := true
-AFTERLIFE_GAPPS := true
+# ArrowOS additions
+TARGET_SUPPORTS_64_BIT_APPS := true
+DEVICE_MAINTAINER := Rinto
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Build info
