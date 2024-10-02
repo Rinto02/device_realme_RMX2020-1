4d6f07cd4eec7d145dc4117734f335f2ad225055
凛岡村リント <138510229+Rinto02@users.noreply.github.com>
2024-10-02 06:15:40 +0600

    RMX2020: Bringup ArrowOS-Extended
    Signed-off-by: 凛岡村リント <138510229+Rinto02@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/afterlife_RMX2020.mk
-
-COMMON_LUNCH_CHOICES := \
-    afterlife_RMX2020-user \
-    afterlife_RMX2020-userdebug \
-    afterlife_RMX2020-eng
+    $(LOCAL_DIR)/arrow_RMX2020.mk
